2462ecd9f28383138695d9c762b2794f6641f622
nicolaipoehner <nicolai.poehner@uni-wuerzburg.de>
2020-09-01 11:35:40 +0200

    Update

---
+++ scratch3extension.js
@@ -0,0 +1,45 @@
+class Utilities {
+
+  constructor() {}
+
+  getInfo() {
+    return {
+      id: 'utilities',
+      name: 'Utlities',
+
+      color1: '#8BC34A',
+      color2: '#7CB342',
+      color3: '#689F38',
+
+      menuIconURI: icon,
+
+      blocks: [
+        {
+          opcode: 'isExactly',
+
+          blockType: Scratch.BlockType.BOOLEAN,
+
+          text: 'is [A] exactly [B]?',
+          arguments: {
+            A: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'apple'
+            },
+            B: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'APPLE'
+            }
+          }
+        }
+      ]
+    }
+  }
+
+  isExactly({A, B}) {
+    return A === B;
+  }
+
+}
+
+Scratch.extensions.register(new Utilities());
+
